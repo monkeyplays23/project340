936c7229e3ca630ddb035f3ef75f6bf5223dd63f
Samuel Murrah <murrahs@flip2.engr.oregonstate.edu>
2022-11-10 14:46:08 -0800

    update all

--- app.js
+++ app.js
@@ -12,55 +12,50 @@ var db = require('./database/db-connector');
 // Handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');   
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');
 
 
 // query
-app.get('/', function(req, res)
-    {  
-        let query1 = "SELECT * FROM Customers;";               // Define our query
+app.get('/', function (req, res) {
+    let query1 = "SELECT * FROM Customers;";               // Define our query
 
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+    db.pool.query(query1, function (error, rows, fields) {    // Execute the query
 
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
-        })                                                      // an object where 'data' is equal to the 'rows' we
-    });                                                         // received back from the query
+        res.render('index', { data: rows });                  // Render the index.hbs file, and also send the renderer
+    })                                                      // an object where 'data' is equal to the 'rows' we
+});                                                         // received back from the query
 
 
 // POST ROUTES
-app.post('/add-person-ajax', function(req, res) 
-{
+app.post('/add-person-ajax', function (req, res) {
     let data = req.body;
 
-    let cemail = data.email;
-    if (!cemail)
-    {
+    let cemail = parseInt(data.email);
+    if (isNaN(cemail)) {
         cemail = 'NULL'
     }
 
 
     query1 = `INSERT INTO Customers (cust_first_name, cust_last_name, cust_email ) VALUES ('${data.fname}', '${data.lname}', ${email})`;
-    db.pool.query(query1, function(error, rows, fields){
+    db.pool.query(query1, function (error, rows, fields) {
 
         if (error) {
 
             console.log(error)
             res.sendStatus(400);
         }
-        else
-        {
+        else {
             query2 = `SELECT * FROM Customers;`;
-            db.pool.query(query2, function(error, rows, fields){
+            db.pool.query(query2, function (error, rows, fields) {
 
                 if (error) {
-                    
+
                     console.log(error);
                     res.sendStatus(400);
                 }
 
-                else
-                {
+                else {
                     res.send(rows);
                 }
             })
@@ -68,35 +63,9 @@ app.post('/add-person-ajax', function(req, res)
     })
 });
 
-app.post('/add-person-form', function(req, res){
-    let data = req.body;
-
-    let cemail = data['input-email'];
-    if (!cemail)
-    {
-        cemail = 'NULL'
-    }
-
-
-    query1 = `INSERT INTO Customers (cust_first_name, cust_last_name, cust_email) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${email})`;
-    db.pool.query(query1, function(error, rows, fields){
-
-        if (error) {
-
-            console.log(error)
-            res.sendStatus(400);
-        }
-
-        else
-        {
-            res.redirect('/');
-        }
-    })
-})
-
 
 // listener 
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
